d33cd9cf583fff5201624bb5010286641954c6ed
Florian Blandin <pro.blandinf@gmail.com>
2020-05-07 17:39:14 +0200

    Initial Commit

---
+++ Kigo-iPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kigo-iPhone
+//
+//  Created by Florian on 07/05/2020.
+//  Copyright © 2020 blandinf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
