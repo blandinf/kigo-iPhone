ae79a988b161898cce938956582dc0c66a4c30f5
Florian Blandin <pro.blandinf@gmail.com>
2020-06-10 17:36:23 +0200

    make extension : from hex to UIColor

---
+++ Kigo-iPhone/utils.swift
@@ -0,0 +1,20 @@
+//
+//  utils.swift
+//  Kigo-iPhone
+//
+//  Created by Florian on 09/06/2020.
+//  Copyright © 2020 blandinf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utils {
+    static func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+}
